4a8061bbd45bd36e5279543375291d4300411d54
RCH <rchillard@gmail.com>
2020-02-19 21:56:27 -0600

    Fix naming collision in ids of slot and dialog elements of reference

--- slot.js
+++ slot.js
@@ -1,9 +1,10 @@
+// slot.js file contents
 customElements.define(
   "my-slot-example",
   class extends HTMLElement {
     constructor() {
       super();
-      let template = document.getElementById("slot-example");
+      let template = document.getElementById("slot-ex");
       let templateContent = template.content;
       const shadowRoot = this.attachShadow({ mode: "open" }).appendChild(
         templateContent.cloneNode(true)
