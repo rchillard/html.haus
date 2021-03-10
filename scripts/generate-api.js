6c010a25c8e64f6873a2b6ff018d883ec3788d71
Ryan Hillard <rchillard@gmail.com>
2021-03-10 03:33:18 -0600

    Add generate API script

---
+++ scripts/generate-api.js
@@ -0,0 +1,38 @@
+// This script will take a local reference.html and generate both JSON and HTML files from it
+
+const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+JSDOM.fromFile("reference.html", { contentType: "text/html" }).then(dom => {
+    var articles = dom.window.document.querySelectorAll('article');
+    
+    articles.forEach(function (article) {
+        
+        var divNodeList = article.querySelectorAll('div');
+        var tempElement = {
+            "name": article.id,
+            // "type": "",
+            "description": divNodeList[1].textContent.replace(/(\r\n|\n|\r)/gm, "").trim(),
+            "code": `https://html.haus/elements/${article.id}.html`,
+            // TBD - need to solve nested list problem and fix reference.html
+            // "attributes": attributeObject,
+            "url": `https://html.haus/reference.html#${article.id}`
+        };
+
+        // Generate JSON portion of API
+        fs.writeFile(`${article.id}.json`, JSON.stringify(tempElement), function (err) {
+            if (err) return console.log(err);
+            console.log(`Successfully created: ${article.id}.json`);
+        });
+
+        // Generate HTML portion of API
+        var codeBlock = article.querySelector('.display');
+        if (codeBlock) {
+            fs.writeFile(`${article.id}.html`, codeBlock.innerHTML.replace(/^\s+/mg, ""), function (err) {
+                if (err) return console.log(err);
+                console.log(`Successfully created: ${article.id}.html`);
+            });
+        }
+    })
+});
